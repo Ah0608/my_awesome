8ac83035914e17331fe307024aa21cdc3374e760
Ah0608 <sc302973@163.com>
2024-04-25 09:27:06 +0800

    修改async_client请求客户端

--- asyncDemo/async_client.py
+++ asyncDemo/async_client.py
@@ -1,28 +1,43 @@
 import asyncio
+from asyncio import sleep
 
 
 class async_request:
-    def __init__(self,max_concurrency=5):
-        self.semaphore = asyncio.Semaphore(max_concurrency)
-        self.queue = []
+    def __init__(self, max_concurrency=5):
+        self.max_workers = max_concurrency
+        self.queue = asyncio.Queue()
 
-    async def fetch(self, function,args,kwargs):
-        async with self.semaphore:
-            await function(*args, **kwargs)
+    async def fetch(self, ):
+        while True:
+            if self.queue.qsize() == 0:
+                exit()
+            item = await self.queue.get()
+            function, line, args, kwargs = item
+            await function(line, *args, **kwargs)
 
-    async def request(self,):
-        tasks = []
-        for item in self.queue:
-            function, args, kwargs = item
-            task = asyncio.ensure_future(self.fetch(function,args,kwargs))
-            tasks.append(task)
-        await asyncio.gather(*tasks)
+    async def request(self, ):
+        workers = [self.fetch() for _ in range(self.max_workers)]
+        print(len(workers))
+        await asyncio.gather(*workers)
 
-    def run(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.gather(self.request()))
+    async def add_task(self, function, data, *args, **kwargs):
+        for line in data:
+            await self.queue.put((function, line, args, kwargs))
+        await self.request()
 
-    def add_task(self, function, *args, **kwargs):
-        if kwargs is None:
-            kwargs = {}
-        self.queue.append((function, args, kwargs))
+
+async def Hello(message, st):
+    print(message, st)
+    await sleep(2)
+
+
+async def main():
+    urls = ['你好{}'.format(i) for i in range(1, 10000)]
+    client = async_request(max_concurrency=5)
+    await client.add_task(Hello, urls, '哈哈')
+
+
+if __name__ == '__main__':
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(asyncio.gather(main()))
+    asyncio.run(main())
