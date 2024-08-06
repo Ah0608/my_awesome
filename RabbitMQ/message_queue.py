384dc56e9f237072271caa079c662be8ccf6aba8
Ah0608 <sc302973@163.com>
2024-08-06 11:48:53 +0800

    RabbitMQ 修改消费者功能

--- RabbitMQ/message_queue.py
+++ RabbitMQ/message_queue.py
@@ -2,10 +2,10 @@ import pika
 
 
 class RabbitMQ(object):
-    def __init__(self,host='192.168.1.149',port=5672,username='user_157',password='go_data_2333'):
-
+    def __init__(self, host='192.168.1.149', port=5672, username='user_157', password='go_data_2333'):
         credentials = pika.PlainCredentials(username, password)
-        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host,port,heartbeat=0, credentials=credentials))
+        self._connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host, port, heartbeat=0, credentials=credentials))
         self._channel = self._connection.channel()
 
     def __enter__(self):
@@ -30,7 +30,7 @@ class Producer(RabbitMQ):
         """
         super(Producer, self).__init__()
         self._queue_name = queue_name
-        self._channel.queue_declare(queue=self._queue_name, durable=True)
+        self._channel.queue_declare(queue=queue_name, durable=True)
 
     def send_message(self, message):
         self._channel.basic_publish(exchange='', routing_key=self._queue_name, body=message,
@@ -58,20 +58,17 @@ class Consumer(RabbitMQ):
             else:
                 break
 
-    def read_message(self,function, args=(), kwargs=None):
-        # 设置每次只从队列中取一个 URL 进行处理
+    def read_message(self, callback):
+        # 设置每次只从队列中取一个消息 进行处理
         self._channel.basic_qos(prefetch_count=1)
         # 指定回调函数，处理收到的消息
-        self._channel.basic_consume(queue=self._queue_name, on_message_callback=function)
-        return self._channel
-
-
-class Count(RabbitMQ):
-    def __init__(self, queue_name):
-        super(Count, self).__init__()
-        self._queue_name = queue_name
-        self.method_frame = self._channel.queue_declare(queue=self._queue_name, durable=True)
+        self._channel.basic_consume(queue=self._queue_name, on_message_callback=callback)
+        self._channel.start_consuming()
 
     def query_counts(self):
-        message_count = self.method_frame.method.message_count
-        return message_count
+        queue = self._channel.queue_declare(queue=self._queue_name, passive=True)
+        return queue.method.message_count
+
+    def clear_queue(self):
+        self._channel.queue_purge(queue=self._queue_name)
+        print(f"队列 {self._queue_name} 所有消息已被清空.")
